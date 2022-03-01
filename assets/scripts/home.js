17512fcbe626a64151fa3169af76026e043789ea
aufaf29 <13518009@std.stei.itb.ac.id>
2022-03-02 01:19:31 +0700

    fux bug and change some text

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -4,12 +4,43 @@ var typewriter = new Typewriter(writerarea, {
     loop: true
 });
 
-typewriter.typeString('Hello World!')
-    .pauseFor(2500)
+document.getElementById("modals-container").innerHTML = 
+    `
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+
+typewriter
+    .typeString('We provide every technology that you need.')
+    .pauseFor(1000)
     .deleteAll()
-    .typeString('Strings can be removed')
-    .pauseFor(2500)
-    .deleteChars(7)
-    .typeString('<strong>altered!</strong>')
-    .pauseFor(2500)
+    .typeString('<strong>Machine Learning</strong>')
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString('<strong>IoT</strong>')    
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString('<strong>Web 3.0</strong>')
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString('<strong>Big Data!</strong>')
+    .pauseFor(2000)
+    .deleteAll()
+
     .start();
--- assets/scripts/todos.js
+++ assets/scripts/todos.js
@@ -36,7 +36,7 @@ function printTODO() {
                     <div class="row">
                         <div class="todo-task col-11">
                             <div class="form-check">
-                                <input class="form-check-input rounded-0" type="checkbox" value="checked" id="${'todo-' + i}" ${(element.checked) ? "checked" : ""}>
+                                <input class="form-check-input rounded-0" type="checkbox" value="" id="${'todo-' + i}" onchange="checkVal(event)" ${(element.checked) ? 'checked' : ''}>
                                 <label class="form-check-label" for="${'todo-' + i}">
                                     ${(element.checked) ? "<s>"+element.task+"</s>" : element.task}
                                 </label>
@@ -53,8 +53,8 @@ function printTODO() {
     
 }
 
-function addTODOS() {
-
+function addTODOS(e) {
+    e.preventDefault();
     
     let todos = getTODOS() == null ? [] : getTODOS();
     todos.push({
@@ -65,9 +65,34 @@ function addTODOS() {
     $("#newtodo").val("");
 }
 
-$(document).ready(() => {
-    
-    
-    printTODO();
+function enterTODOS(e) {
+    console.log(e.keyCode)
+    if(e.keyCode == 13){
+        e.preventDefault(); 
 
+        let todos = getTODOS() == null ? [] : getTODOS();
+        todos.push({
+            "task": $("#newtodo").val(),
+            "checked": false
+        })
+        setTODOS(todos);
+        $("#newtodo").val("");
+    }
+    
+}
+
+function checkVal(e) {
+    e.preventDefault();
+    let num = parseInt(e.target.id.split("-")[1]);
+    
+    let todos = getTODOS() == null ? [] : getTODOS();
+    todos[num] = {
+        "task": todos[num].task,
+        "checked": e.target.checked
+    }
+    setTODOS(todos);
+}
+
+$(document).ready(() => {
+    printTODO();
 })
